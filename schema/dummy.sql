ed9e9e7f98008f730fbb9841c2e3d054fb13b63a
bbearcookie <aodem@naver.com>
2024-06-23 23:37:22 +0900

    chore: docker-compose 및 DDL 작성

---
+++ schema/dummy.sql
@@ -0,0 +1,3 @@
+INSERT INTO Hello (title, content) VALUES ('Hello', 'World1');
+INSERT INTO Hello (title, content) VALUES ('Bye', 'World2');
+INSERT INTO Hello (title, content) VALUES ('Good', 'World3');
