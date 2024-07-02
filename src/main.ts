18692f9ea7756c69eb1d12c738b115f4978baf04
DongjaJ <jdh981118@naver.com>
2024-07-02 17:29:11 +0900

    fix: 스웨거 문구 변경

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('TMI HOMERS API')
+    .setDescription('API Documentation')
+    .setVersion('0.0.0')
+    .addTag('TMI HOMERS')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(5010);
 }
