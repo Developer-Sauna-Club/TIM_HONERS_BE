cc8bbe4aa93abd0de0b503b59712970994db5ae0
bbearcookie <aodem@naver.com>
2024-07-21 20:09:34 +0900

    chore: 환경 변수 분기에 cross-env 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { HelloModule } from './hello/hello.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       expandVariables: true,
       isGlobal: true,
     }),
