d005008e0d95dc730252dedf813cb74e3a5be5e1
bbearcookie <aodem@naver.com>
2024-08-04 12:27:18 +0900

    chore: User 테이블에 email 필드를 unique로 수정

--- schema/ddl.sql
+++ schema/ddl.sql
@@ -9,7 +9,7 @@ CREATE TABLE `Hello` (
 
 CREATE TABLE `User` (
     _id INT AUTO_INCREMENT,
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     fullName VARCHAR(255),
     username VARCHAR(255),
