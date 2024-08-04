cc8ee11d4266c3dfffc0ec49a988723d91ff2a37
bbearcookie <aodem@naver.com>
2024-08-04 14:55:08 +0900

    refactor: password 필터링 방식 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,21 +6,13 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async getUserByEmail(email: string) {
-    return this.prisma.user.findUnique({
-      select: {
-        id: true,
-        email: true,
-        password: false,
-        fullName: true,
-        username: true,
-        profileImage: true,
-        coverImage: true,
-        createdAt: true,
-        updatedAt: true,
-      },
+    const user = await this.prisma.user.findUnique({
       where: {
         email,
       },
     });
+
+    const { password, ...result } = user;
+    return result;
   }
 }
