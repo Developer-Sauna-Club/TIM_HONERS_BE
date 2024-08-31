ffcd3589ed54b534d8189d2e7f4ab546810bd284
daeun <gbajs754@gmail.com>
2024-08-31 14:48:58 +0900

    feat : req -> req.user로 수정

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -11,12 +11,12 @@ export class SettingsController {
   @Put('/update-user')
   @UseGuards(JwtAuthGuard)
   updateUser(@Body() updateUserDto: UpdateUserDto, @Req() req) {
-    return this.settingsService.updateUser(req.id, updateUserDto);
+    return this.settingsService.updateUser(req.user.id, updateUserDto);
   }
 
   @Put('/update-password')
   @UseGuards(JwtAuthGuard)
   updatePassword(@Body() updatePasswordDto: UpdatePasswordDto, @Req() req) {
-    return this.settingsService.updatePassword(req.id, updatePasswordDto);
+    return this.settingsService.updatePassword(req.user.id, updatePasswordDto);
   }
 }
