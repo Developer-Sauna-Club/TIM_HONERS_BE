fc0e884ca4551111217743d6c61ac2d8ce211e43
DongjaJ <jdh981118@naver.com>
2024-09-08 21:45:36 +0900

    fix: update dto 추가

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsNotEmpty()
+  @IsNumber()
+  postId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  channelId: number;
+
+  @IsOptional()
+  @IsString()
+  imageToDeletePublicId?: string;
+}
